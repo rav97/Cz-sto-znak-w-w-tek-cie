bf007b393cb90804c0e50f31058e14550cc02a72
rav97 <35076475+rav97@users.noreply.github.com>
2018-12-08 13:07:29 +0100

    Add files via upload
    All project

---
+++ Częstość/Częstość/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Częstość
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ReadFile
+            // 
+            this.ReadFile.Location = new System.Drawing.Point(13, 13);
+            this.ReadFile.Name = "ReadFile";
+            this.ReadFile.Size = new System.Drawing.Size(235, 33);
+            this.ReadFile.TabIndex = 0;
+            this.ReadFile.Text = "Wczytaj plik";
+            this.ReadFile.UseVisualStyleBackColor = true;
+            this.ReadFile.Click += new System.EventHandler(this.ReadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Plik:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(13, 70);
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Size = new System.Drawing.Size(235, 20);
+            this.path.TabIndex = 2;
+            this.path.TextChanged += new System.EventHandler(this.path_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Znak";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Częstość";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(175, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Procent";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 135);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(236, 118);
+            this.output.TabIndex = 6;
+            this.output.Text = "";
+            this.output.TextChanged += new System.EventHandler(this.output_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 265);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ReadFile);
+            this.Name = "Form1";
+            this.Text = "Częstość";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReadFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox output;
+    }
+}
+
---
+++ Częstość/Częstość/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Częstość
+{
+    public partial class Form1 : Form
+    {
+        public string filepath, inputText;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filepath = openFileDialog1.FileName;
+            }
+            path.Text=filepath;
+            if (filepath != null)
+            {
+                FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+                try
+                {
+                    StreamReader sr = new StreamReader(fs);
+                    inputText = sr.ReadToEnd();
+                    sr.Close();
+                }
+                catch (Exception ex){
+                    MessageBox.Show(ex.ToString());
+                }
+                string input = inputText.ToUpper();
+                output.Text = "";
+                zliczaj(input);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void path_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zliczaj(string txt)
+        {
+            IDictionary<char, int> znakIle = new Dictionary<char, int>();
+            float inputSize;
+            inputSize = (float)txt.Length;
+
+            for(int i=0;i<txt.Length;i++)
+            {
+                if (znakIle.ContainsKey(txt[i]))
+                    znakIle[txt[i]] += 1;
+                else
+                    znakIle[txt[i]] = 1;
+            }
+            print(znakIle, inputSize);
+        }
+        private void print(IDictionary<char,int> dict, float size)
+        {
+            float percent;
+            dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            foreach (KeyValuePair<char,int> kvp in dict)
+            {
+                if (kvp.Key < 'A' | kvp.Key > 'Z')
+                    continue;
+                percent = ((float)dict[kvp.Key] / size) * 100;
+                output.Text = output.Text + kvp.Key + "\t\t" + dict[kvp.Key] + "\t\t" + (int)percent + "\n";
+            }
+        }
+    }
+}
